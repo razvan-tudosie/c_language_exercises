d5aa1fa42bd3f1b40ec46719af5bddcde9f5ee6e
Razvan Tudosie <tudosie.razvan@gmail.com>
2020-10-26 08:42:45 +0200

    update ignore

--- beginner/tempCodeRunnerFile.c
+++
@@ -1,9 +0,0 @@
-    // // get num1 and num2 values from user
-    // printf("Introdu primul numar intreg din trei: \n");
-    // scanf("%d", &numbers[0]);
-
-    // printf("Introdu al doilea numar intreg din trei: \n");
-    // scanf("%d", &numbers[1]);
-    
-    // printf("Introdu al treilea numar intreg din trei: \n");
-    // scanf("%d", &numbers[2]);

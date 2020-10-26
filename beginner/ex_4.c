a3fd203c2c5a1420bef8683f08981917b78894fc
Razvan Tudosie <tudosie.razvan@gmail.com>
2020-10-26 08:39:44 +0200

    update code

---
+++ beginner/ex_4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+// ##############################
+// Scrieți un program care citește de la tastatură un număr natural reprezentând un an și
+// verifică dacă anul respectiv este bisect sau nu. Un an este bisect fie dacă este divizibil
+// cu 4 și nu este divizibil cu 100, fie este divizibil cu 400.
+// ##############################
+
+int main() {
+
+
+    return 0;
+}
